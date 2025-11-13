3f63a1a74157c33e256a24c80ecb816c20961879
Emiliano Montes <louieliet@vyvo.tech>
2025-11-13 11:37:55 -0600

    Initial Commit

---
+++ pantheronwellness/pantheronwellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  pantheronwellnessApp.swift
+//  pantheronwellness
+//
+//  Created by Emiliano Montes on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct pantheronwellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
